f12078bd2e90053961df50778ee279b4663edaca
Vito Medlej <vito.medlej@gmail.com>
2024-12-24 23:07:36 +0300

    Added allowed cors

--- src/server.ts
+++ src/server.ts
@@ -16,13 +16,30 @@ import { productsRouter } from "./api/products/productsRouter";
 const logger = pino({ name: "server start" });
 const app: Express = express();
 
+const allowedOrigins = [
+    'https://ecommerce-dashboard-template.vercel.app',
+    `https://ecom-template-roan.vercel.app/`
+  ];
+
 // Set the application to trust the reverse proxy
 app.set("trust proxy", true);
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(
+    cors({
+      origin: function (origin, callback) {
+       
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true, 
+    })
+  );
 app.use(helmet());
 app.use(rateLimiter);
 app.use(authenticateRequest)
